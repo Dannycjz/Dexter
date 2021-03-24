99f520fbb83e3baf6c8f22543be6ed8627153c8c
Marco Wang <marcowang01@gmail.com>
2021-03-24 18:25:15 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from pygooglenews import GoogleNews
+
+
+def main():
+    print("hello world")
+
+    gn = GoogleNews()
+
+    top = gn.top_news()
+
+    for i in range(10):
+        print(top['entries'][i])
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
