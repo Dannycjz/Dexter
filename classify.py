a59d456112c3440dfcaf2985530ccf2647b4a586
Marco Wang <marcowang01@gmail.com>
2021-03-29 18:42:31 +0800

    ML skeleton

--- classify.py
+++ classify.py
@@ -1,13 +1,38 @@
-import datetime
-import pickle
 import pandas as pd
-from FedTools import MonetaryPolicyCommittee
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import RegexpTokenizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics import classification_report
+from tqdm import tqdm
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn import metrics
+import random
+
 
 STOCK_SYMBOL = "SPY"
+TAGS = ["vg", "g", "n", "b", 'vb']  # v = very, g = good, b = bad, n = neutral
 
 
 def classify():
-    read_data()
+    data = read_data()
+
+    tokens = RegexpTokenizer(r'[a-zA-Z]+')
+    cv = CountVectorizer(tokenizer=tokens.tokenize, stop_words="english", ngram_range=(1, 1))
+    text_counts = cv.fit_transform(tqdm(data['content']))
+    tfidf_counts = TfidfTransformer().fit_transform(text_counts)
+
+    RANDOM_STATE = 999
+    X_train, X_test, y_train, y_test = train_test_split(
+        text_counts, data['tags'], test_size=0.3, random_state=RANDOM_STATE)
+
+    clf = AdaBoostClassifier().fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    print(F"{accuracy:.2%} - {STOCK_SYMBOL}")
+
+    print(classification_report(y_test, y_pred, target_names=TAGS))
 
 
 def read_data():
@@ -27,15 +52,34 @@ def read_data():
         # TODO: possible soln: keep incrementing days forward/backward until valid
 
         stock = STOCK_SYMBOL
+        temp = [2, -2]
+        r_num = random.random() * random.choice(temp)
 
-        delta = 10  # TODO: change this
-        tags.append(delta)
+        delta = r_num  # TODO: change this. if possible store this as % change
 
-    data.insert(5, "tag", tags, True)
+        BIG_VAL = 1
+        SMALL_VAL = 0.1
+
+        if delta > BIG_VAL:
+            tag = TAGS[0]
+        elif delta > SMALL_VAL:
+            tag = TAGS[1]
+        elif delta < -1 * BIG_VAL:
+            tag = TAGS[4]
+        elif delta < -1 * SMALL_VAL:
+            tag = TAGS[3]
+        else:
+            tag = TAGS[2]
+
+        tags.append(tag)
+
+    data.insert(5, "tags", tags, True)
     data.to_pickle("main_dataset.pkl")
 
     for i in range(5):
-        print(F"{data['date'][i]}: {data['tag'][i]}\t|\t{data['title'][i]}")
+        print(F"{data['date'][i]}: {data['tags'][i]}\t|\t{data['title'][i]}")
+
+    return data
 
 
 if __name__ == '__main__':
