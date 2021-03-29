c18bb17a2bf2a75aa2842d8f2a8690055d695ccb
Marco Wang <marcowang01@gmail.com>
2021-03-29 19:05:41 +0800

    ML skeleton

--- classify.py
+++ classify.py
@@ -4,6 +4,7 @@ from nltk.tokenize import RegexpTokenizer
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics import classification_report
+from nltk import sent_tokenize
 from tqdm import tqdm
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn import metrics
@@ -17,18 +18,25 @@ TAGS = ["vg", "g", "n", "b", 'vb']  # v = very, g = good, b = bad, n = neutral
 def classify():
     data = read_data()
 
+    print("\nTable info")
+    data.info()  # prints table structure to terminal
+
     tokens = RegexpTokenizer(r'[a-zA-Z]+')
-    cv = CountVectorizer(tokenizer=tokens.tokenize, stop_words="english", ngram_range=(1, 1))
+    cv = CountVectorizer(tokenizer=tokens.tokenize, stop_words="english", ngram_range=(1, 2))
+
+    print("\nGenerating bag of words:")
     text_counts = cv.fit_transform(tqdm(data['content']))
     tfidf_counts = TfidfTransformer().fit_transform(text_counts)
+    print(F"Matrix size: {text_counts.shape}")
 
-    RANDOM_STATE = 999
+    RANDOM_STATE = 123
     X_train, X_test, y_train, y_test = train_test_split(
-        text_counts, data['tags'], test_size=0.3, random_state=RANDOM_STATE)
+        text_counts, data['tag'], test_size=0.3, random_state=RANDOM_STATE)
 
     clf = AdaBoostClassifier().fit(X_train, y_train)
     y_pred = clf.predict(X_test)
 
+    print("\nResults:")
     accuracy = metrics.accuracy_score(y_test, y_pred)
     print(F"{accuracy:.2%} - {STOCK_SYMBOL}")
 
@@ -38,7 +46,7 @@ def classify():
 def read_data():
     print("reading data...")
     data = pd.read_json("scraping/powell_data.json")
-    data.info()  # prints table structure to terminal
+
     tags = []
     for i in data.index:
         date_str = str(data["date"][i])
@@ -73,11 +81,27 @@ def read_data():
 
         tags.append(tag)
 
-    data.insert(5, "tags", tags, True)
+    data.insert(5, "tag", tags, True)
+
+    sentence_list = []
+    for i in range(len(data)):
+        sentences = sent_tokenize(data['content'][i])
+        for s in sentences:
+            temp_dict = {
+                'date': data['date'][i],
+                'title': data['title'][i],
+                'content': s,
+                'tag': data['tag'][i]
+            }
+            sentence_list.append(temp_dict)
+
+    data = pd.DataFrame(sentence_list)
+
     data.to_pickle("main_dataset.pkl")
 
-    for i in range(5):
-        print(F"{data['date'][i]}: {data['tags'][i]}\t|\t{data['title'][i]}")
+    print("\n5 docs:")
+    for i in [0, 1000, 2000, 3000, 4000, 5000]:
+        print(F"{data['date'][i]}: {data['tag'][i]}  \t|  {data['title'][i]}")
 
     return data
 
